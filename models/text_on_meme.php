ff33579d05378387c76946c3c3acc0753fd5f7eb
juliedi <julie.di@codeur.online>
2017-12-20 11:20:17 +0100

    ajout ligne texte du bas

--- models/text_on_meme.php
+++ models/text_on_meme.php
@@ -5,15 +5,19 @@ header ("Content-type: image/jpeg");
 $image = imagecreatefromjpeg("hackfimu.jpg");
 $size = 45;
 $angle = 0;
-$x = 80;
-$y = 80;
+$x = 80; //positionne le 1er caractère du texte de droite à gauche
+$y = 80;// positionne le 1er caractère du texte en partant du haut
+$xx = 500;
+$yy = 650;
 $color = imagecolorallocate($image, 255, 255, 255);/*Couleur de la police*/
 $font = "Rubik-Bold.ttf";//la notice préconise de ne pas mettre le nom de l'extension ".ttf" mais ne fonctionne pas sans l'ajouter
-$text = "C'est quand que ça s'arrête?";
+$text1 = "oh yeuuu";
+$text2 = "ben alooors";
 
 putenv('GDFONTPATH=' . realpath('.'));
 
-$text1 = imagettftext($image, $size, $angle, $x, $y, $color, $font, $text);
+$toptext = imagettftext($image, $size, $angle, $x, $y, $color, $font, $text1);
+$bottomtext = imagettftext($image, $size, $angle, $xx, $yy, $color, $font, $text2);
 imagejpeg($image);
 ?>
 
