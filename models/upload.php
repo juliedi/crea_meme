441424c1a7f3887819d7abe17ace38e237999b42
juliedi <julie.di@codeur.online>
2017-12-20 10:45:36 +0100

    ajout police.ttf

--- models/upload.php
+++ models/upload.php
@@ -1,38 +1,58 @@
 <?php
-$fileToUpload = $_FILES['fileToUpload'];
-$dossier = 'upload/'.$_FILES['fileToUpload']['name'];
-$fichier = basename($_FILES['fileToUpload']['name']);
-$taille_maxi = 100000;
-$taille = filesize($_FILES['fileToUpload']['tmp_name']);
-$extensions = array('.png', '.gif', '.jpg', '.jpeg');
-$extension = strrchr($_FILES['fileToUpload']['name'], '.'); 
-//Début des vérifications de sécurité...
-if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
-{
-     $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg,...';
+
+$description = $_POST["description"];
+
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
 }
-if($taille>$taille_maxi)
-{
-     $erreur = 'Le fichier est trop gros...';
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
 }
-if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
-{
-     //On formate le nom du fichier ici...
-     $fichier = strtr($fichier, 
-          'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
-          'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
-     $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
-     if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
-     {
-          echo 'Upload effectué avec succès !';
-     }
-     else //Sinon (la fonction renvoie FALSE).
-     {
-          echo 'Echec de l\'upload !';
-     }
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
 }
-else
-{
-     echo $erreur;
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
 }
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (basename($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]) . " has been uploaded.";
+
+        $withoutext = explode(".", basename($_FILES["fileToUpload"]["name"]));
+        
+        $nom = $target_dir . $withoutext[0] . " - " . date('d-m-Y à H\hi\ms\s') . "." . $imageFileType;
+        echo "<hr>";
+        $resultat = move_uploaded_file($_FILES['fileToUpload']['tmp_name'],$nom);
+        if ($resultat){
+            echo "Transfert réussi" ;
+            include("connexion.php");
+        }
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+
 ?>
