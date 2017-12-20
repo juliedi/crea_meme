3a19c68d4964362af04e669720bcb200575ac1c8
EvaGms <eva.g@codeur.online>
2017-12-20 09:23:49 +0100

    index

--- index.php
+++ index.php
@@ -1,12 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Page test</title>
-</head>
-<body>
-    <img src="mon_image.php>" alt="">
-</body>
-</html>
+<?php
+
+//$BASE_URL = "http://localhost/BddMVC/";
+
+if (!isset($_GET['route']) || empty($_GET['route']))
+{
+   $route = 'home';
+}
+
+else {
+   $route = $_GET['route'];
+}    
+
+if (empty($route)) {
+
+}
+
+switch ($route) {
+   case 'home':
+       include("views/homeview.php");
+       break;
+
+    case 'meme':
+       include("views/memeView.php");
+       break;
+
+/*    case ('preg_match('#film/[0-9]#',$route) ? true : false'):
+        $id = $route[5];
+        include("controllers/filmController.php");
+        break;
+*/
+   default:
+       include("controllers/".$route."Controller.php");
+       break;
+}
+?>
